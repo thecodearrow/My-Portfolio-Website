3238f28d7b16bb5b5d42345cfd03cf72461d5941
Prashanth Vaidya <prashanthvaidya@Prashanths-MacBook-Air.local>
2020-06-14 03:44:17 +0530

    Updates

---
+++ precache-manifest.d30f79fe01f72e0c8b1b9f644d7115e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00425610d26cf4d8d54327b5c9ee493b",
+    "url": "/Personal-Website-2020/src/index.html"
+  },
+  {
+    "revision": "cce6267870f8312da199",
+    "url": "/Personal-Website-2020/src/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3aa7379e70b0368e4693",
+    "url": "/Personal-Website-2020/src/static/js/2.adaffb31.chunk.js"
+  },
+  {
+    "revision": "11cfc49acd1819f3e804b64601daa210",
+    "url": "/Personal-Website-2020/src/static/js/2.adaffb31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cce6267870f8312da199",
+    "url": "/Personal-Website-2020/src/static/js/main.6e239958.chunk.js"
+  },
+  {
+    "revision": "028edcd28b700c8f87d3",
+    "url": "/Personal-Website-2020/src/static/js/runtime-main.668bcee3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Personal-Website-2020/src/static/media/logo.5d5d9eef.svg"
+  }
+]);
