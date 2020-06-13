2badb5153f0a35a87b257e29a3664b6930919359
Prashanth Vaidya <prashanthvaidya@Prashanths-MacBook-Air.local>
2020-06-14 03:29:51 +0530

    Updates

---
+++ precache-manifest.6ea6c4b5a37672ee783d3f63a10e8154.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb79e603166c50a5e184099693cc94b",
+    "url": "/dev/index.html"
+  },
+  {
+    "revision": "2e36549e32ef1bafc28c",
+    "url": "/dev/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "01823632b1bfbbd48a8b",
+    "url": "/dev/static/js/2.34fbff9e.chunk.js"
+  },
+  {
+    "revision": "11cfc49acd1819f3e804b64601daa210",
+    "url": "/dev/static/js/2.34fbff9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e36549e32ef1bafc28c",
+    "url": "/dev/static/js/main.b68123ba.chunk.js"
+  },
+  {
+    "revision": "7ae6aff56211af84c07b",
+    "url": "/dev/static/js/runtime-main.ee392d26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dev/static/media/logo.5d5d9eef.svg"
+  }
+]);
